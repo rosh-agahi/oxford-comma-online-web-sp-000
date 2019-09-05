28d989778c50dafbf9b0b53410d45fabc92585fb
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 15:38:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
 if array.size == 2
   array.join(" and ")
 else array.size > 2 
-  array[0..-2].join(', ') + " and " + array[-1]
+  array[0..-2].join(', ') + ", and " + array[-1]
 end
 end
