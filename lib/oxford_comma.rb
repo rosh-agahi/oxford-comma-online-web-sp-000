6f111098b7370d7819991e1f6495e43aef86bfac
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 15:39:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+if array.size < 2
+  array.join
+elsif array.size == 2
+  array.join(" and ")
+else array.size > 2 
+  array[0..-2].join(', ') + ", and " + array[-1]
+end
 end
